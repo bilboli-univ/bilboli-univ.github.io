34552aa72d0102727a695b09f26c102fa56452a1
bilboli-univ <basile.lebrun-billot@etu.umontpellier.fr>
2025-12-21 12:48:39 +0100

    Fix formatting in firebase-config.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -2,7 +2,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDe3Mpmcd5zNZz7CJ0RHP1RxeM8tA_-aLM",
   authDomain: "findmeonphoto.firebaseapp.com",
   projectId: "findmeonphoto",
-  storageBucket: "findmeonphoto.appspot.com"
+  storageBucket: "findmeonphoto.appspot.com",
   messagingSenderId: "78625732870",
   appId: "1:78625732870:web:5d0c849462c83df2220687",
   measurementId: "G-BMBGZBB1PF"
